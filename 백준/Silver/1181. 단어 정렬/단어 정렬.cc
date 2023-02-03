8ab0c089e8bf86bc1875063bcf8864516543b751
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-02-03 12:14:59 +0900

    [Silver V] Title: 단어 정렬, Time: 1256 ms, Memory: 4532 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<pair<int, string>> v;
-vector <string> s;
+vector<string> v;
 
-
+bool compare(string a, string b) {
+	if (a.length() == b.length()) { //길이가 같으면 
+		return a < b; //사전순으로 a보다 b가 더 크면 a가 앞에 위치.
+	}
+	return a.length() < b.length(); // 길이가 다르면 , 길이순으로 a보다 b가 더 크면 a가 앞에 위치
+}
 
 int main(){
 
@@ -11,17 +15,16 @@ int main(){
 	for (int i = 0; i < num; i++) {
 		string str; cin >> str;
 
-		if (find(s.begin(), s.end(), str) == s.end()) { // 중복되는 값은 안 넣음.
-			s.push_back(str);
-			v.push_back(make_pair(str.size(), str));
+		if (find(v.begin(), v.end(), str) == v.end()) { // 중복되는 값은 안 넣음.
+			v.push_back(str);
 		}
 
 	}
 
-	sort(v.begin(), v.end());
+	sort(v.begin(), v.end(),compare);
 
 	for (int i = 0; i < v.size(); i++) {
-		cout << v[i].second << '\n';
+		cout << v[i]<< '\n';
 	}
 
 	return 0;
