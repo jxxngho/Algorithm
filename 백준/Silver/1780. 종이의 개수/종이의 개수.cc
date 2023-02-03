e5c5184e4a95926be3653fb1300f8e5abc04e90a
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-02-03 10:24:29 +0900

    [Silver II] Title: 종이의 개수, Time: 344 ms, Memory: 20720 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,44 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 int arr[2188][2188];
-int ans[3];
+int ans[3]; // [0] = -1, [1] = 0 , [2] = 1
+
 void divide_conquer(int num, int x, int y) {
 
 	int tmp = arr[y][x];
 	bool same = true;
-	if (num == 1) {
+	if (num == 1) { // 쪼개서 하나만 남은 상황
 		same = true;
 	}
-	else {
+	else { 
 		for (int i = y; i < num + y; i++) {
 			for (int j = x; j < num + x; j++) {
-				if (arr[i][j] != tmp) {
+				if (arr[i][j] != tmp) { // 다른 숫자가 있는 경우
 					same = false;
 					break;
 				}
 			}
 		}
 	}
-	if (same) {
+	if (same) { // 만약 모두 숫자가 같더라면
 		ans[tmp+1]++;
 	}
 	else {
-		divide_conquer(num / 3, x,y);
-		divide_conquer(num / 3, x+num/3, y);
-		divide_conquer(num / 3, x + 2* (num / 3), y);
+		divide_conquer(num / 3, x,y); // 1번
+		divide_conquer(num / 3, x+num/3, y); // 2번
+		divide_conquer(num / 3, x + 2* (num / 3), y); // 3번
 
-		divide_conquer(num / 3, x, y + num / 3);
-		divide_conquer(num / 3, x + num / 3, y + num / 3);
-		divide_conquer(num / 3, x + 2 * (num / 3), y + num / 3);
+		divide_conquer(num / 3, x, y + num / 3); // 4번
+		divide_conquer(num / 3, x + num / 3, y + num / 3); // 5번
+		divide_conquer(num / 3, x + 2 * (num / 3), y + num / 3); // 6번
 
-		divide_conquer(num / 3, x, y +2*( num / 3));
-		divide_conquer(num / 3, x + num / 3, y + 2*(num / 3));
-		divide_conquer(num / 3, x + 2 * (num / 3), y + 2 * (num / 3));
+		divide_conquer(num / 3, x, y +2*( num / 3)); // 7번
+		divide_conquer(num / 3, x + num / 3, y + 2*(num / 3)); // 8번
+		divide_conquer(num / 3, x + 2 * (num / 3), y + 2 * (num / 3)); //9번
 	}
 
 }
-
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
