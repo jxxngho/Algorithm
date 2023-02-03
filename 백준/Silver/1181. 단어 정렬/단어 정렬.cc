f06316f578cea166b3ca3c0924aa158340503219
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-02-03 12:15:56 +0900

    [Silver V] Title: 단어 정렬, Time: 1208 ms, Memory: 4648 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -10,6 +10,9 @@ bool compare(string a, string b) {
 }
 
 int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
 	int num; cin >> num;
 	for (int i = 0; i < num; i++) {
