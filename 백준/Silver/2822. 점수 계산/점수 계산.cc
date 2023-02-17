5787f01f0b44f5279d74e7e524947718bf1a3980
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-02-17 17:54:27 +0900

    [Silver V] Title: 점수 계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.cc
+++ 백준/Silver/2822. 점수 계산/점수 계산.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector <pair<int, int>> v;
+vector <pair<int, int>> v(8);
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -8,8 +8,8 @@ int main() {
 	cout.tie(NULL);
 	int tmp;
 	for (int i = 0; i < 8; i++) {
-		cin >> tmp;
-		v.push_back(make_pair(tmp, i+1));
+		cin >> v[i].first;
+		v[i].second = i + 1;
 	}
 
 	sort(v.begin(), v.end(),greater<>());
