b93c5d8b36a226bf17a75c71e1f5d90d0d94bda8
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-06-22 13:38:00 +0900

    [Gold V] Title: 행복 유치원, Time: 196 ms, Memory: 6280 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.cc
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[300001];
-vector <int> v;
+int child[300001];
+vector <int> interval;
 int main() {
     int N, K;
     cin >> N >> K;
@@ -9,21 +9,21 @@ int main() {
     for (int i = 0; i < N; i++) {
         int tmp;
         cin >> tmp;
-        arr[i] = tmp ;
+        child[i] = tmp;
     }
     if (K == 1) { // K==1일 경우
-        cout << arr[N-1] - arr[0] << endl;
+        cout << child[N-1] - child[0] << endl;
         return 0;
     }
     for (int i = 1; i < N; i++) { // 인접한 차이 값 저장
-        v.push_back(arr[i] - arr[i - 1]);
+        interval.push_back(child[i] - child[i - 1]);
     }
-    sort(v.begin(), v.end());
+    sort(interval.begin(), interval.end());
 
     
     int sum = 0;
     for (int i = 0; i < N- K ; i++) { // 제일 많이 차이나는 것 제외
-        sum += v[i];
+        sum += interval[i];
     }
     cout << sum << endl;
     return 0;
