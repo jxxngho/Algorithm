800f58777b0866f64722382aa488bf340de69c77
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-10-09 13:38:44 +0900

    [Silver V] Title: 다리 놓기, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	int test_case;
+	cin >> test_case;
+
+	for (int i = 0; i < test_case; i++) {
+		int a, b;
+		long long result = 1; // 결과값 
+		cin >> a >> b;
+		if (a == 1) { // 왼쪽 다리 하나일 경우
+			cout << b << endl;
+		}
+		else if (a == b) { // 양쪽 다리 갯수 같을 경우
+			cout << 1 << endl;
+		}
+		else {
+
+			int r = 1; // nCr 
+			for (int i = b; i > b - a; i--) {
+				result *= i;
+				result /= r;
+				r++;
+			}
+			cout << result << endl;
+		}
+	}
+}
+
