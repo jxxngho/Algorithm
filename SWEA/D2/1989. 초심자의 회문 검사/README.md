be51a7824b81925f5dc0a1f73e25f829ee5cbfcf
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-10-29 23:06:36 +0900

    [D2] Title: 초심자의 회문 검사, Time: 6 ms, Memory: 13,540 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/README.md
@@ -0,0 +1,15 @@
+# [D2] 초심자의 회문 검사 - 1989 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PyTLqAf4DFAUq) 
+
+### 성능 요약
+
+메모리: 13,540 KB, 시간: 6 ms, 코드길이: 449 Bytes
+
+### 제출 일자
+
+2023-10-29 23:06
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int cnt = 0;
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++) {
+		string str;
+		cin >> str;
+		int flag = 1; 
+
+		for (int i = 0; i < str.size() / 2; i++) { // 비교
+			if (str[i] == str[str.size() - 1 - i]) { 
+				continue;
+			}
+			else {
+				flag = 0;
+			}
+		}
+		cout << "#" << i + 1 << " " << flag << endl;
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
