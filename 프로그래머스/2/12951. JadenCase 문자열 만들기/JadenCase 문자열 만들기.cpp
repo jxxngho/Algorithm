85f88ee35a154537fd7c88eb5cb86b459313b11d
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-10-30 13:31:33 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -8,15 +8,14 @@ string solution(string s) {
     string tmp="";
     for(int i=0;i<s.size();i++){    
         tmp+=s[i];
-        if(s[i]==' '||i==s.size()-1){
-            for(int j=0;j<tmp.size();j++){
-                if(tmp[j]>='A'&&tmp[j]<='Z'){
+        if(s[i]==' '||i==s.size()-1){ //공백을 만났을 때, 맨 끝일 때
+             if(tmp[0]>='a'&&tmp[0]<='z'){ // 맨 첫번째 문자가 소문자인 경우 
+                tmp[0] -=32; // 대문자 변환
+             }
+             for(int j=1;j<tmp.size();j++){
+                if(tmp[j]>='A'&&tmp[j]<='Z') // 대문자 -> 소문자변환
                     tmp[j]+=32;
-                }
-            }
-            if(tmp[0]>='a'&&tmp[0]<='z'){
-                tmp[0] -=32;
-            }
+            } // for
             answer += tmp;
             tmp="";
         } // if
