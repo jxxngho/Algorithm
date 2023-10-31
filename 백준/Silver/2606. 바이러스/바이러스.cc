01ae2f58da3fea8376fbd261f9c835fc790c113d
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-10-31 20:44:44 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,41 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool visited[101]; //방문 여부
-vector <int> v[101];
 
-int bfs(int start) {
-	queue<int> q;
-	q.push(start); // 첫 노드 queue에 삽입
-	visited[start] = true; // 첫 노드 방문 처리
-	int cnt = 0;
-	while (!q.empty()) {
-		// 큐가 비면 break
-		int x = q.front();
-		cnt++;
-		q.pop();  // 큐에서 원소 하나 빼서 출력
-		for (int i = 0; i < v[x].size(); i++) {
-			int y = v[x][i];
-
-			if (!visited[y]) {
-				q.push(y);
-				visited[y] = true; // 방문처리
-			}
-		}
+vector <int> a[101];
+int visited[101];
+int ans = 0;
+void dfs(int start) {
+	if (visited[start]) return;
+	else {
+		visited[start] = 1;
+		ans++;
 	}
-	return cnt;
+
+	for (int i = 0; i < a[start].size(); i++) {
+		int y = a[start][i];
+		dfs(y);
+	}
 }
 
-
 int main() {
-
-	int com; cin >> com; // 컴퓨터 개수
-	int node; cin >> node; // 노드 개수
-	while (node--) {
-		int x, y;
+	int count_com;
+	cin >> count_com;
+	int edge;
+	cin >> edge;
+	int x, y;
+	for (int i = 0; i < edge; i++) {
 		cin >> x >> y;
-		v[x].push_back(y);
-		v[y].push_back(x);
+		a[x].push_back(y);
+		a[y].push_back(x);
 	}
-	cout << bfs(1) - 1 << endl;
+
+	dfs(1);
+	cout << ans - 1;
 	return 0;
-} //main
+}
