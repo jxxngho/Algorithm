46f1d205e02b2737f14c619b3d2e3356644c2e7b
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-11-01 15:00:37 +0900

    [level 2] Title: 구명보트, Time: 1.56 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(vector<int> people, int limit) {
     int answer = 0;
     sort(people.begin(),people.end()); // 정렬
-    int flag = 0;
+    int flag = 0; // 최솟값 체크
     for(int i=people.size()-1;flag <=i;i--){
        if(people[i]+people[flag]<=limit){
            flag++;
