33a0785020c84af70972b9f8b1c43fb79d4592fc
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-11-08 13:29:22 +0900

    [Silver II] Title: 섬의 개수, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,52 +1,53 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int arr[51][51] = { 0, };
+int map[51][51] = { 0, };
 int visited[51][51] = { 0, };
-int w, h; // w : 너비 , h : 높이
-int dx[8] = { -1,-1,-1,0,0,1,1,1 }, dy[8] = { -1,0,1,-1,1,-1,0,1 };
+int dy[8] = { 0,1,1,1,0,-1,-1,-1 }, dx[8] = { 1,1,0,-1,-1,-1,0,1 }; //우,우하,하,좌하,좌,좌상,상,우상
+int w, h; // w: 너비, h: 높이
 void dfs(int y, int x) {
-	for(int i=0;i<8;i++){
+	for (int i = 0; i < 8; i++) {
 		int dfs_x = x + dx[i];
 		int dfs_y = y + dy[i];
-		if (dfs_x < 0 || dfs_y < 0 || dfs_x >= w || dfs_y >= h) continue; // 범위 check
-		if (!visited[dfs_y][dfs_x] && arr[dfs_y][dfs_x] == 1) { 
+		if (dfs_x < 0 || dfs_y < 0 || dfs_x >= w || dfs_y >= h) continue; // 지도범위안 
+		if (map[dfs_y][dfs_x] == 1 && visited[dfs_y][dfs_x] == 0) { // 섬 && 방문 안한 곳
 			visited[dfs_y][dfs_x] = 1;
 			dfs(dfs_y, dfs_x);
 		}
-
-
 	}
 }
 
 
-int main() {	
+int main() {
 	while (true) {
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+		int ans = 0; // 섬개수
 		cin >> w >> h;
-		if (w == 0 && h == 0) return 0;
-		for (int i = 0; i < h; i++) {
+		if (w == 0 && h == 0) return 0; // 종료조건
+
+		for (int i = 0; i < h; i++) { // 값 대입
 			for (int j = 0; j < w; j++) {
 				int tmp;
 				cin >> tmp;
-				arr[i][j] = tmp;
-			}
-		} // 초기화부분
-		int ans = 0;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if (visited[i][j] == 0 && arr[i][j] ==1) {
-					visited[i][j] = 1;
-					ans++;
-					dfs(i, j);
-				}
+				map[i][j] = tmp;
 			}
 		}
+		
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if (map[i][j] == 1 && !visited[i][j]) { // 섬이고 방문안했을 경우
+					dfs(i, j);
+					ans++;
+				}			
+			}
+		} 
 		cout << ans << endl;
-		memset(arr, 0, sizeof(arr));
+		
+		// 0으로 초기화
+		memset(map, 0, sizeof(map));
 		memset(visited, 0, sizeof(visited));
 
-
-
-	}
-	
-}
+	} // while
+} // main
