9e4f189a42612f13578f792a56d62140d19a151b
jxxngho <109848417+jxxngho@users.noreply.github.com>
2023-11-21 11:46:32 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -1,42 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+string str = "", ans = "", tmp = "";
+int  cnt = 0;
 
-int main() {
-	string str = "";
-	cin >> str;
 
-	// AAAA , BB 
-
-	string ans = "";
-	string tmp = "";
-	int  cnt = 0;
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] == 'X') cnt++;
-		else {
-			if (cnt % 2 != 0) { // 못 만드는 경우
-				cout << -1 << '\n';
-				return 0;
-			}
-			else { // 만들 수 있는 경우
-				int A_cnt = cnt / 4;
-				int B_cnt = (cnt - 4 * A_cnt) / 2;
-				for (int j = 0; j < A_cnt * 4; j++) ans += "A";
-				for (int j = 0; j < B_cnt * 2; j++) ans += "B";
-				ans += ".";
-				cnt = 0;
-			}
-		}
-	}
-	if (cnt %2!= 0) {
+int  Polyomino(int cnt) {
+	if (cnt % 2 != 0) { // 못 만드는 경우
 		cout << -1 << '\n';
 		return 0;
 	}
-	else {
+	else { // 만들 수 있는 경우
 		int A_cnt = cnt / 4;
 		int B_cnt = (cnt - 4 * A_cnt) / 2;
 		for (int j = 0; j < A_cnt * 4; j++) ans += "A";
 		for (int j = 0; j < B_cnt * 2; j++) ans += "B";
+
 	}
+	return 1;
+}
+
+int main() {
+	cin >> str;
+
+	// AAAA , BB 
+
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == 'X') cnt++;
+		else {
+			if (!Polyomino(cnt))return 0;
+			ans += ".";
+			cnt = 0;
+		}
+	}
+	if (!Polyomino(cnt)) return 0;
 	
 	cout << ans << '\n';
 	return 0;
